c9700ba22ef117a86cf57593bd8e63a00eaa251a
H4r1 <156511786+MaDDoX-h4r1-4926@users.noreply.github.com>
2024-02-14 14:58:07 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureInjection();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
   runApp(const MainApp());
 }
 
