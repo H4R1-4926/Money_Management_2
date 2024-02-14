5b5244b9c390d0ce83a8be8461dadde9fdd4689a
H4r1 <156511786+MaDDoX-h4r1-4926@users.noreply.github.com>
2024-02-14 14:58:52 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:fire_base_project/Presentation/Home/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
