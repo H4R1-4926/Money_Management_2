aa6bf698bfe253ea34940c6df40647ca397ae447
H4r1 <harigovindappu4926@gmail.com>
2024-07-17 13:23:53 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:fire_base_project/Application/bloc/home_bloc.dart';
 import 'package:fire_base_project/Domain/Dependancy_injection/injectable.dart';
 import 'package:fire_base_project/Presentation/Home/homepage.dart';
+import 'package:fire_base_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -8,7 +10,9 @@ import 'package:get/get.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureInjection();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -19,8 +23,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => getIt<HomeBloc>(),
-      child: const GetMaterialApp(
-          debugShowCheckedModeBanner: false, home: HomePage()),
+      child: GetMaterialApp(
+          theme: ThemeData(useMaterial3: false),
+          debugShowCheckedModeBanner: false,
+          home: const HomePage()),
     );
   }
 }
